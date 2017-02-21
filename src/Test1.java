35a60397310a9bc1eea9c7869f83b98ec137dc9a
labesalena <alenalabes@gmail.com>
2017-02-21 19:22:10 +0300

    add class test2

--- src/Test1.java
+++ src/Test1.java
@@ -4,6 +4,10 @@
 public class Test1 {
         public static void main (String[] args) {
             System.out.println("Hello, World");
+            Test2 test = new Test2();
+            System.out.println(test);
+            System.out.println(test.a);
+
         }
     }
 
---
+++ src/Test2.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ilya on 21.02.2017.
+ */
+public class Test2 {
+    int a = 5;
+
+    @Override
+    public String toString() {
+        return "" + (a + 100) ;
+    }
+}
